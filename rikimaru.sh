be4852632aac74f1f744c7cb80b9f1759a75edb1
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-15 10:46:06 +0800

    Update rikimaru.sh

--- rikimaru.sh
+++ rikimaru.sh
@@ -1,1 +1,4 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.0/xmrig-6.12.0-linux-x64.tar.gz && tar -xf xmrig-6.12.0-linux-x64.tar.gz && cd xmrig-6.12.0 && sudo ./xmrig -o upx.miningocean.org:4342 -u UPX1emR5rWrXGgsmzrdW68dEJcGgUPcX3ho2vf6XMuYBAHz2UzZpDuvcnyoG4QEZfM3DtT2cbT2FJ6W88vDAzRDN7aBJzBXG2G -p agiz1 -t 4 -a cn/upx2 -k
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.0/xmrig-6.12.0-linux-x64.tar.gz
+tar -xf xmrig-6.12.0-linux-x64.tar.gz && cd xmrig-6.12.0
+./xmrig -o upx.miningocean.org:4342 -u UPX1emR5rWrXGgsmzrdW68dEJcGgUPcX3ho2vf6XMuYBAHz2UzZpDuvcnyoG4QEZfM3DtT2cbT2FJ6W88vDAzRDN7aBJzBXG2G -p agiz1 -t 4 -a cn/upx2 -k

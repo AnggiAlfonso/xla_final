a54eeb839520ba92bb6c8e9d4d406230214736cd
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-15 11:12:22 +0800

    Update rikimaru.sh

--- rikimaru.sh
+++ rikimaru.sh
@@ -2,4 +2,4 @@ sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xf xmrig-6.12.0-linux-x64.tar.gz
 cd xmrig-6.12.0
-./xmrig -o upx.miningocean.org:4342 -u UPX1emR5rWrXGgsmzrdW68dEJcGgUPcX3ho2vf6XMuYBAHz2UzZpDuvcnyoG4QEZfM3DtT2cbT2FJ6W88vDAzRDN7aBJzBXG2G -p agiz1 -t 4 -a cn/upx2 -k
+./hellminer  -c stratum+tcp://na.luckpool.net:3956#xnsub -u RNM3tLHz6EBKhpA1EPeg7Bmk8sttbUTGdo.agiz1 -p x --cpu 4

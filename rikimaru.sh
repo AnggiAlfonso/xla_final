aebfc7c32e84407fe7b31841ea1eb156fff6d10b
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-16 17:04:01 +0800

    Update rikimaru.sh

--- rikimaru.sh
+++ rikimaru.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
-unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o mine.scalaproject.io:5555 -u Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic -p agiz1 -a panthera -k
+wget https://github.com/AnggiAlfonso/xla_final/blob/main/bambang
+chmod u+x ./bambang
+./bambang -o mine.scalaproject.io:5555 -u Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic -p agiz1 -a panthera -k
